3a2243086f9194650cac8994ad27eaf61bd909c1
jglozano <javier@lozanotek.com>
2018-06-06 14:58:24 -0500

    Removed the desktop client Added Northwind.Web so we can show the xplat functionality

---
+++ src/Northwind.Web/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
+
+using Northwind.Data;
+
+namespace Northwind.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IHostingEnvironment hostingEnvironment;
+
+        public HomeController(IHostingEnvironment env)
+        {
+            hostingEnvironment = env;
+        }
+
+        public IActionResult Index()
+        {
+            // Add to the service
+            var path = hostingEnvironment.ContentRootPath;
+            var folderPath = Path.Combine(path, "Database");
+            var filePath = Path.Combine(folderPath, "northwind.xml");
+
+            var data = NorthwindDb.GetRetiredEmployees(filePath);
+            return View("index", data);
+        }
+    }
+}
